d2930d232d8ca2fa14318eb9f7f41695bd312c13
Rob Data <roberthernandezmarketing@gmail.com>
2025-05-22 05:48:31 -0400

    Adavantage

--- advantage_validation.py
+++ advantage_validation.py
@@ -1,6 +1,24 @@
+from fastapi import FastAPI
 from pydantic import BaseModel, Field, EmailStr, field_validator
 import re
 
+app = FastAPI()
+
 class Usuario(BaseModel):
   nombre : str = Field(..., min_length=3, max_length=50)
-  email = 
+  email : EmailStr
+  edad : int = Field(..., gt=0, lt=120)
+  codigo_postal : str
+
+  @field_validator('codigo_postal')
+  def validar_codigo_postal(cls, v):
+    if not re.match(r'^\d{5}$', v):
+        raise ValueError('Código postal debe tener 5 digitos')
+    return v
+  
+@app.post("/usarios/")
+def crear_usuario(usuario: Usuario):
+   return {
+      "mensaje" : "Usuario creado exitosamente", 
+      "datos"   : usuario
+   }
